673239c51025b1ebbb21cedc5d6b2e11b70a2f99
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-14 16:44:44 +0530

    Delete day-5-3-exercise.py

--- day-5-3-exercise.py
+++
@@ -1,7 +0,0 @@
-#Write your code below this row 👇
-alternative_sum = 0
-for number in range(1, 101):
-  if number % 2 == 0:
-    # print(number)
-    alternative_sum += number
-print(alternative_sum)
