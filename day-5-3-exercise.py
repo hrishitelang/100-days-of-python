6317713ef8f3d7dc0653b84e456164f2f3482e1e
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-14 16:44:31 +0530

    Add files via upload

---
+++ day-5-3-exercise.py
@@ -0,0 +1,7 @@
+#Write your code below this row 👇
+alternative_sum = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    # print(number)
+    alternative_sum += number
+print(alternative_sum)
