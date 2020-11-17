736fd0062093f898de60d72da3c2a89af69a2328
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-17 17:10:46 +0530

    Add files via upload

---
+++ Day 8/day-8-start.py
@@ -0,0 +1,14 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+  print("Hi I'm Hrishi.")
+  print("I'm 21 years old.")
+  print("I love reading and writing.")
+
+def greet_with(name, location):
+  print(f"Hi I'm {name}")
+  print(f"I'm from {location}")
+
+greet_with(location="Mumbai", name="Hrishi")
