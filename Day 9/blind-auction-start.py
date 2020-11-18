85748bb064726747cdedf96bdf892482ccf83944
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-11-18 16:15:42 +0530

    Add files via upload

--- Day 9/blind-auction-start.py
+++ Day 9/blind-auction-start.py
@@ -1,20 +1,6 @@
 from replit import clear
 from art import logo
 #HINT: You can call clear() to clear the output in the console.
-print(logo)
-print("Welcome to the secret auction program.")
-flag = True
-auction = {}
-while flag:
-  name = input("What is your name?: ")
-  bid = int(input("What's your bid?: $"))
-  auction[name] = bid
-  choice = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
-  if choice == "no":
-    flag = False
-  elif choice == "yes":
-    clear()
-
 def winner(auction):
   winner = ''
   max = 0
@@ -25,4 +11,17 @@ def winner(auction):
   #winner = max(auction, key=auction.get)
   print(f"The winner is {winner} with a bid of ${max}")
 
-winner(auction)
+print(logo)
+print("Welcome to the secret auction program.")
+flag = True
+auction = {}
+while flag:
+  name = input("What is your name?: ")
+  bid = int(input("What's your bid?: $"))
+  auction[name] = bid
+  choice = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+  if choice == "no":
+    flag = False
+    winner(auction)
+  elif choice == "yes":
+    clear()
