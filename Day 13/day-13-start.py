a0bc6f723a85dd455a531858bfa1adb8dd1ac4d5
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-03 23:05:28 +0530

    Add files via upload

--- Day 13/day-13-start.py
+++ Day 13/day-13-start.py
@@ -1,18 +1,18 @@
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  for i in range(1, 20):
-    i+=1
-    if i == 20:
-      print("You got it")
-my_function()
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     i+=1
+#     if i == 20:
+#       print("You got it")
+# my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num-1])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
