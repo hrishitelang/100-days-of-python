2020512c65ad9a62fa0e734556b68458c57c056c
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-05 17:37:01 +0530

    Add files via upload

--- Day 15/main.py
+++ Day 15/main.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -30,14 +31,7 @@ resources = {
     "coffee": 100,
 }
 
+
 # TODO: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
 # TODO: Turn off the Coffee Machine by entering “off” to the prompt.
 # TODO: Print report.
-# TODO: Check resources sufficient?
-# TODO: Process coins.
-# TODO: Check transaction successful?
-# TODO: Make Coffee.
-
-while True:
-
-
