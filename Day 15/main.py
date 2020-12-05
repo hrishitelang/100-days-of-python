3938e5ad2052fb21dff8b97d5c927998ecc383f6
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-05 17:38:46 +0530

    Add files via upload

--- Day 15/main.py
+++ Day 15/main.py
@@ -60,3 +60,6 @@ def coins():
     penny = float(input("how many pennies? "))
     money = (quarter * 0.25) + (dime * 0.10) + (nickle * 0.05) + (penny * 0.01)
     return money
+
+
+# TODO: Check transaction successful?
