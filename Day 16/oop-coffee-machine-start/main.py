edc7bb15a1e03d276b6df60686fe668c801edecf
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-06 14:43:08 +0530

    Add files via upload

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -3,7 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-menuitem = MenuItem()
+menu_item = MenuItem()
 coffee = CoffeeMaker()
 money = MoneyMachine()
 
+cost = 0
+while True:
+    flag = 0
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if choice == "report":
+        coffee.report()
+    elif choice == "off":
+        break
+    else:
+        if coffee.is_resource_sufficient(choice):
+            continue
+        y = money.process_coins()
+        if money.make_payment():
+            continue
+        else:
+            coffee.make_coffee(menu_item)
