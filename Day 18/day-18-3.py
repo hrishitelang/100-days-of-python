c67964b33d8bdf01bdbe6f717f6ccf3adc984dd8
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-08 23:29:46 +0530

    Add files via upload

--- Day 18/day-18-3.py
+++ Day 18/day-18-3.py
@@ -1,10 +1,12 @@
-from turtle import Turtle, Screen
+from random import choice
+from turtle import Turtle, Screen, color
 
 timmy = Turtle()
 
-i = 3
+color = ['#006400', '#4682B4', '#0000FF', '#8B0000', '#FA8072', '#4B0082', '#32CD32', '#ADFF2F', '#FF0000']
 #timmy.setposition(0,100)
 for i in range(3, 10):
+    timmy.color(choice(color))
     for _ in range (i):
         timmy.forward(100)
         timmy.right(360/i)
