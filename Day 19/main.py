4606ff01cd38bcfe1a015a2a43b1e0452b0ed41c
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-21 17:27:56 +0530

    Add files via upload

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,38 +1,18 @@
 from turtle import Turtle, Screen
-import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
-user_input = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-# print(user_input)
-race = False
+screen.bgcolor("black")
+screen.title("My Snake Game")
 
-colors = ["red", "orange", "blue", "yellow", "green", "blue", "purple"]
-all_turtle = []
-y = 0
-for i in colors:
-    turtle = Turtle(shape="turtle")
-    turtle.color(i)
-    turtle.penup()
-    turtle.goto(x=-280, y=y - 130)
-    all_turtle.append(turtle)
-    y += 50
-
-if user_input:
-    race = True
-
-while race:
-    for turtle in all_turtle:
-        if turtle.xcor() > 280:
-            race = False
-            winner = turtle.fillcolor()
-            if winner == user_input:
-                print(f"You've won! The {winner} turtle is the winner!")
-            else:
-                print(f"You've lost! The {winner} turtle is the winner!")
-
-        turtle.forward(random.randint(0, 10))
-
+x = 0
+for i in range(3):
+    timmy = Turtle(shape="square")
+    timmy.color("white")
+    timmy.penup()
+    timmy.setposition(x, 0)
+    timmy.pendown()
+    x -= 20
 
 
 screen.exitonclick()
