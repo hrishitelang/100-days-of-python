38e155bd3d9f15a209ef8e52b39867ff3c8522a0
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2020-12-31 23:56:03 +0530

    Add files via upload

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,18 +1,27 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-x = 0
-for i in range(3):
-    timmy = Turtle(shape="square")
-    timmy.color("white")
-    timmy.penup()
-    timmy.setposition(x, 0)
-    timmy.pendown()
-    x -= 20
+snake = Snake()
 
+# for i in range(3):
+#     timmy = Turtle(shape="square")
+#     timmy.color("white")
+#     timmy.penup()
+#     timmy.setposition(x, 0)
+#     segments.append(timmy)
+#     x -= 20
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
