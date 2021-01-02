f60b6d93b175dc369779393754e6076ac6d0e38c
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-02 23:56:34 +0530

    Add files via upload

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,5 +1,8 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+
+from Ball import Ball
 from paddle import Paddle
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,25 +12,6 @@ screen.tracer(0)
 
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
-# tim = Turtle()
-# tim.shape("square")
-# tim.color("white")
-# tim.shapesize(5, 1)
-# tim.penup()
-# tim.goto(350,0)
-# screen.update()
-
-#
-# def up():
-#     new_y = tim.ycor() + 10
-#     tim.goto(tim.xcor(), new_y)
-#
-# def down():
-#     new_y = tim.ycor() - 10
-#     tim.goto(tim.xcor(), new_y)
-
-# paddle = Paddle()
-# paddle.go(350, 0)
 
 screen.listen()
 screen.onkeypress(r_paddle.up, "Up")
@@ -35,8 +19,13 @@ screen.onkeypress(r_paddle.down, "Down")
 screen.onkeypress(l_paddle.up, "w")
 screen.onkeypress(l_paddle.down, "s")
 
+ball = Ball()
+
 flag = True
+
 while flag:
-    screen.tracer(1)
+    ball.move()
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
