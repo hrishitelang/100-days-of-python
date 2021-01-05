2f9726a7175cf65360b8367171a08eeb0c1bd04c
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-05 12:53:18 +0530

    Add files via upload

---
+++ Day 24/snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        for i in range(3):
+            self.create_segment()
+        self.head = self.segments[0]
+
+    def create_segment(self):
+        x = 0
+        timmy = Turtle(shape="square")
+        timmy.color("white")
+        timmy.penup()
+        timmy.setposition(x, 0)
+        self.segments.append(timmy)
+        x -= 20
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
