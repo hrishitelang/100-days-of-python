b0e8a1770da3ce3bf79f4a5666063b9072a0e110
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-05 12:51:30 +0530

    Add files via upload

---
+++ Day 24/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+
+from food import Food
+from scoreboard import ScoreBoard
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.create_segment()
+        score.diditeat()
+
+    # Detect Collision with food
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        game_is_on = False
+        score.printgameover()
+
+    # Detect collision with tail
+    for x in snake.segments:
+        if x == snake.head:
+            pass
+        elif snake.head.distance(x) < 10:
+            game_is_on = False
+            score.printgameover()
+
+screen.exitonclick()
