6cda956718603e2e6e15d620d865f76e7552033b
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-07 12:01:32 +0530

    Add files via upload

--- Day 25/main.py
+++ Day 25/main.py
@@ -17,10 +17,8 @@ import csv
 #     #     print(row[1])
 #     print(temperatures)
 #
-import pandas
 
-data = pandas.read_csv('weather_data.csv')
-# # print(data['temp'])
+# print(data['temp'])
 # temp_list = data['temp'].to_list()
 # average1 = sum(data['temp'])/len(data)
 # average2 = sum(temp_list)/len(temp_list)
@@ -30,7 +28,12 @@ data = pandas.read_csv('weather_data.csv')
 # print(average2)
 
 # print(data[data.temp == data['temp'].max()])
-monday = data[data['day'] == 'Monday']
-monday_temp = monday['temp']
-monday_temp = (monday_temp * 9/5) + 32
-print(monday_temp)
+# monday = data[data['day'] == 'Monday']
+# monday_temp = monday['temp']
+# monday_temp = (monday_temp * 9/5) + 32
+# print(monday_temp)
+import pandas
+
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+squirrel_df = pandas.DataFrame(data['Primary Fur Color'].value_counts())
+squirrel_df.to_csv("squirrel_count.csv")
