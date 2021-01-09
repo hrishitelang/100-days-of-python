06f343647e8854acdad4e1b917aa8936eb189b22
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-09 22:55:53 +0530

    Add files via upload

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -6,39 +6,61 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 5
+SHORT_BREAK_MIN = 2
 LONG_BREAK_MIN = 20
 reps = 1
+marks = ""
+timer = None
+
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer_clock)
+    timer.config(text="Timer")
+    canvas.itemconfig(timer_number, text="00:00")
+    checkmark.config(text="", fg=GREEN, bg=YELLOW)
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
-    while reps < 9:
-        if reps in [1, 3, 5, 7]:
-            count_down(25*60)
-        elif reps in [2, 4, 6]:
-            count_down(5*60)
-        elif reps == 8:
-            count_down(20*60)
-        reps += 1
+    if reps % 8 == 0:
+        timer.config(text="Break", fg=RED)
+        count_down(LONG_BREAK_MIN)
+    elif reps % 2 == 0:
+        timer.config(text="Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN)
+    elif reps % 2 == 1:
+        timer.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN)
+    reps += 1
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
+    global marks
+    global timer_clock
     count_minute = math.floor(count/60)
     count_second = count % 60
     if count_second == 0:
         count_second = "00"
-    elif count_second in range(1,10):
+    elif count_second in range(1, 10):
             count_second = "0"+str(count_second)
     canvas.itemconfig(timer_number, text=f'{count_minute}:{count_second}')
     if count > 0:
-        window.after(1000, count_down, count-1)
+        timer_clock = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            marks += "✔"
+        checkmark.config(text=marks)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Application")
-window.config(padx=100, pady=50,bg=YELLOW)
-
+window.config(padx=100, pady=50, bg=YELLOW)
 
 timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 38))
 timer.grid(row=0, column=1)
@@ -53,10 +75,10 @@ canvas.grid(row=1, column=1)
 start = Button(text="Start", command=start_timer)
 start.grid(row=2, column=0)
 
-reset = Button(text="Reset")
+reset = Button(text="Reset", command=reset_timer)
 reset.grid(row=2, column=2)
 
-checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark = Label(fg=GREEN, bg=YELLOW)
 checkmark.grid(row=3, column=1)
 
 window.mainloop()
