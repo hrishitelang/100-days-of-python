27d88c56fe61d5e0eb89f577bf8def71a269f6c0
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-09 14:05:27 +0530

    Add files via upload

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,26 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Application")
+window.config(padx=100, pady=50,bg=YELLOW)
+
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 38))
+timer.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 125, text="00:00", fill=YELLOW, font=(FONT_NAME, 34, 'bold'))
+canvas.grid(row=1, column=1)
+
+start = Button(text="Start")
+start.grid(row=2, column=0)
+
+reset = Button(text="Reset")
+reset.grid(row=2, column=2)
+
+checkmark = Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmark.grid(row=3, column=1)
+
+window.mainloop()
