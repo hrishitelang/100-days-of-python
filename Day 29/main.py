8888f0a51d767d370e7d6c1b593d8a93482dbb5a
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-10 22:01:18 +0530

    Add files via upload

--- Day 29/main.py
+++ Day 29/main.py
@@ -2,6 +2,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_text():
+    email_answer = email_text.get()
+    password_answer = password_text.get()
+    name_answer = name_text.get()
+
+    with open('data.txt', 'a') as file:
+        file.write(f"{name_answer} | {email_answer} | {password_answer}\n")
+    name_text.delete(0, 'end')
+    email_text.delete(0, 'end')
+    password_text.delete(0, 'end')
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -19,23 +29,25 @@ name_label.grid(row=2, column=1)
 
 name_text = Entry(width=39)
 name_text.grid(row=2, column=2, columnspan=2)
+name_text.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=3, column=1)
 
 email_text = Entry(width=39)
 email_text.grid(row=3, column=2, columnspan=2)
+email_text.insert(0,"telang.hrishikesh@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=4, column=1)
 
-password_text = Entry(width=21)
+password_text = Entry(width=21, textvariable=StringVar())
 password_text.grid(row=4, column=2)
 
 generate_password = Button(text="Generate Password")
 generate_password.grid(row=4, column=3)
 
-add = Button(text="Add", width=36)
+add = Button(text="Add", width=36, command=save_text)
 add.grid(row=5, column=2, columnspan=2)
 
 
