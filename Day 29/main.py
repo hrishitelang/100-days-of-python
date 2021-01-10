1d283bb2d53c2a76a0f32005b0f7a3f59baf6414
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-10 23:38:33 +0530

    Add files via upload

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,7 +1,24 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for char in range(nr_letters)]
+    password_list += [random.choice(letters) for char in range(nr_symbols)]
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_text.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_text():
     name_answer = name_text.get()
@@ -57,7 +74,7 @@ password_label.grid(row=4, column=1)
 password_text = Entry(width=21, textvariable=StringVar())
 password_text.grid(row=4, column=2)
 
-generate_password = Button(text="Generate Password")
+generate_password = Button(text="Generate Password", command=generate)
 generate_password.grid(row=4, column=3)
 
 add = Button(text="Add", width=36, command=save_text)
