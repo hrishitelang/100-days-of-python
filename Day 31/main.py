09491e9db7ff5df1e7a9e38d195e6923456ee19c
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-20 10:50:33 +0530

    Add files via upload

--- Day 31/main.py
+++ Day 31/main.py
@@ -6,30 +6,41 @@ import random
 #---------------------------- UI DESIGN ----------------------------#
 
 
+def change_word():
+    global french_word
+    canvas.delete(french_word)
+    french_word = canvas.create_text(400, 263, text=data['French'].iloc[random.randint(0, 100)], font=('Arial', 40, "bold"))
+
+
+#---------------------------- UI DESIGN ----------------------------#
+
 
 window = Tk()
 window.config(width=1000, height=1000, padx=50, pady=50, bg=BACKGROUND_COLOR)
 window.title('Flashy')
 canvas = Canvas(width=800, height=525, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=1, column=1, columnspan=2)
 
 card_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_image)
-canvas.grid(row=1, column=1, columnspan=2)
-
-language = Label(text="Title", justify=CENTER,font=('Arial', 40, "italic"), bg='white')
-language.place(x=340, y=100)
+canvas.create_text(400, 100, text='French', font=('Arial', 40, "italic"))
 
 data = pd.read_csv('data/french_words.csv')
-#word = data['French'].iloc[random.randint(0,100)]
-french_word = Label(text='word', justify=CENTER, font=('Arial', 60, "bold"), bg='white')
-french_word.place(x=300, y=263)
+word = data['French'].iloc[random.randint(0, 100)]
+french_word = canvas.create_text(400, 263, text=word, font=('Arial', 40, "bold"))
+
+# language = Label(text="Title", justify=CENTER,font=('Arial', 40, "italic"), bg='white')
+# language.place(x=340, y=100)
+
+# french_word = Label(text='word', justify=CENTER, font=('Arial', 60, "bold"), bg='white')
+# french_word.place(x=300, y=263)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-button1 = Button(image=wrong_image, highlightthickness=0)
-button1.grid(row=2, column=1)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=change_word)
+wrong_button.grid(row=2, column=1)
 
 right_image = PhotoImage(file="images/right.png")
-button2 = Button(image=right_image, highlightthickness=0)
-button2.grid(row=2, column=2)
+right_button = Button(image=right_image, highlightthickness=0, command=change_word)
+right_button.grid(row=2, column=2)
 
 window.mainloop()
