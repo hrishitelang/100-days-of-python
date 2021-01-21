604c3108ae3b586c4a1b944c82b867e060c04d21
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-01-21 12:33:24 +0530

    Add files via upload

--- Day 32/main.py
+++ Day 32/main.py
@@ -1,12 +1,27 @@
-import smtplib
+import smtplib, ssl
 
 my_email = "clareatkins7@gmail.com"
 password = "gigudi1234"
-connection = smtplib.SMTP("smtp.gmail.com")
+PORT = 587
 
+context = ssl.create_default_context()
+connection = smtplib.SMTP("smtp.gmail.com", port=PORT)
 #A way of securing our connection to our email server
-connection.starttls()
+connection.starttls(context=context)
 connection.login(user=my_email, password=password)
-
-connection.sendemail(from_addr=my_email, to_addrs="jeffroberts1003@yahoo.com", msg="Hello Jeff, love you.")
+connection.sendmail(from_addr=my_email, to_addrs="jeffroberts1003@yahoo.com", msg="Hello Jeff, love you.")
 connection.close()
+
+'''
+port = 465  # For SSL
+my_email = "clareatkins7@gmail.com"
+password = "gigudi1234"
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(my_email, password)
+    server.sendmail(from_addr=my_email, to_addrs="jeffroberts1003@yahoo.com", msg="Hello Jeff, love you.")
+'''
+
