f9176cbf832aa3d40d0305e19c17226e04b273fb
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-13 23:18:21 +0530

    Add files via upload

---
+++ Day 33/main.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.get(url='http://api.open-notify.org/iss-now.json')
+response.raise_for_status()
+
+data = response.json()
+# print(data)
+
+latitude = response.json()['iss_position']['latitude']
+longitude = response.json()['iss_position']['longitude']
+
+position = (latitude, longitude)
+
+print(position)
