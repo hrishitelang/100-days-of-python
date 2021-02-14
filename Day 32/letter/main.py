1bf51ed71a8b3f3e910908bca259cfc57637e6db
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-14 22:09:00 +0530

    Update main.py

--- Day 32/letter/main.py
+++ Day 32/letter/main.py
@@ -39,11 +39,3 @@ if today in birthdays_dict:
 
 
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-
-
-
