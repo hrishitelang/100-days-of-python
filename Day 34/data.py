719d7998f3afb381914cab20d29d3d5f21744852
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-15 15:57:28 +0530

    Add files via upload

--- Day 34/data.py
+++ Day 34/data.py
@@ -1,3 +1,14 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    'type': 'boolean'
+}
+response = requests.get('https://opentdb.com/api.php', params=parameters)
+response.raise_for_status()
+question_data = response.json()['results']
+
+'''
 question_data = [
     {
         "category": "Science: Computers",
@@ -100,3 +111,4 @@ question_data = [
         ]
     }
 ]
+'''
