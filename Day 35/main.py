590a64bc6b7c9951b98944332a23e63cb712768d
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-24 21:36:38 +0530

    Add files via upload

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,7 +1,10 @@
+import os
 import requests
 from twilio.rest import Client
 
-KEY = "82dc612e99793a50d4bf7a77f29c6592"
+KEY = os.environ['KEY']
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
 
 parameters = {
     "lat": -2.529450,
@@ -12,9 +15,6 @@ parameters = {
 
 response = requests.get('https://api.openweathermap.org/data/2.5/onecall', params=parameters)
 response.raise_for_status()
-
-account_sid = "AC3c9761c1f610fe96f2c996069774fcab"
-auth_token = "7584fbbdaf5fd06f13593f5d43b68613"
 
 data = response.json()
 print(data)
