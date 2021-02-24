e9bc1e6ad52ddacdd9df21b61f5086172aa7eaa4
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-25 00:49:49 +0530

    Add files via upload

--- Day 39/main.py
+++ Day 39/main.py
@@ -1,8 +1,1 @@
-import numpy as np
-
-a1 = [0, 1000, 2000, 3000]
-a2 = [20, 30, 40, 50]
-
-result = np.vstack((a1,a2))
-
-print(result)
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
