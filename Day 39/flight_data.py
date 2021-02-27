7b0531a0f147a4b5741713fbe07c8ae337ed2b69
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-27 10:24:05 +0530

    Add files via upload

--- Day 39/flight_data.py
+++ Day 39/flight_data.py
@@ -1,3 +1,19 @@
+import requests
+from pprint import pprint
+
+API_ENDPOINT = "https://api.sheety.co/e87c6faabdd61205ee49f30b11c9b043/flightDeals/prices"
+TOKEN = "ksnfkslnksnkfllfkmkmnklsmnks"
+
+header = {
+    "Authorization": f"Bearer {TOKEN}"
+}
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    def __init__(self):
+        self.response = requests.get(url=API_ENDPOINT, headers=header)
+        self.data = self.response.json()
+        pprint(self.data)
+
+
+#flight = FlightData()
