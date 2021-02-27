7adcd29dcc0d123f143619721785aa0812e8890f
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-27 10:24:29 +0530

    Add files via upload

--- Day 39/main.py
+++ Day 39/main.py
@@ -1,1 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_data import FlightData
+from flight_search import FlightSearch
+
+class Flight:
+    def __init__(self):
+        self.flight_data = FlightData()
+        self.flight_search = FlightSearch()
+
+flight = Flight()
+flight_search = FlightSearch()
