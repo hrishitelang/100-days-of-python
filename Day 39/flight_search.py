b0d3829a2f22581b19360bbcfd97607256cc4223
Hrishikesh Telang <65037869+hrishitelang@users.noreply.github.com>
2021-02-27 10:23:52 +0530

    Add files via upload

--- Day 39/flight_search.py
+++ Day 39/flight_search.py
@@ -1,3 +1,24 @@
+import requests
+
+API_ENDPOINT = "https://api.sheety.co/e87c6faabdd61205ee49f30b11c9b043/flightDeals/prices"
+TOKEN = "ksnfkslnksnkfllfkmkmnklsmnks"
+
+header = {
+    "Authorization": f"Bearer {TOKEN}"
+}
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.response = requests.post(url=API_ENDPOINT, json=body, headers=header)
+        print(self.response.text)
+        self.data = self.response.json()
+
+    def inserttesting(self):
+        for i in range(len(self.data['prices'])):
+            body = {
+                "price": {
+                    "iataCode": 'TESTING'
+                }
+            }
+            
